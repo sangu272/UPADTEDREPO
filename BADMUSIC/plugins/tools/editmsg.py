1d2f7d285432a80223d604db66efe48fc9d1c069
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-16 11:29:34 +0530

    Update editmsg.py

--- BADMUSIC/plugins/tools/editmsg.py
+++ BADMUSIC/plugins/tools/editmsg.py
@@ -2,7 +2,7 @@ from BADMUSIC import app
 from pyrogram import Client, filters
 import asyncio
 from pyrogram import *
-@app.on_message(filters.edited)
+@app.on_message(filters.text)
 async def delete_edited_message(client, message):
     # Wait for a specified time (e.g., 10 seconds)
     await asyncio.sleep(10)
