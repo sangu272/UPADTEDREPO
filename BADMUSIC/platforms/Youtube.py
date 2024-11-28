c77f2263083a774c052a7a6d7fe07a1ef179891d
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-28 18:05:22 +0530

    Update Youtube.py

--- BADMUSIC/platforms/Youtube.py
+++ BADMUSIC/platforms/Youtube.py
@@ -10,8 +10,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from BADMUSICMUSIC.utils.formatters import time_to_seconds
-from BADMUSICMUSIC.utils.database import is_on_off
+from BADMUSIC.utils.formatters import time_to_seconds
+from BADMUSIC.utils.database import is_on_off
 
 
 def cookies():
