81d3f6bda9d8d751ed98aa7242693ccaecd9c961
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-12-01 18:53:52 +0530

    Update live.py

--- BADMUSIC/plugins/play/live.py
+++ BADMUSIC/plugins/play/live.py
@@ -4,7 +4,7 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from BADMUSIC import YouTube, app
+from BADMUSIC import Platform, app
 from BADMUSIC.utils.channelplay import get_channeplayCB
 from BADMUSIC.utils.decorators.language import languageCB
 from BADMUSIC.utils.stream.stream import stream
@@ -19,24 +19,24 @@ async def play_live_stream(client, CallbackQuery, _):
     if CallbackQuery.from_user.id != int(user_id):
         try:
             return await CallbackQuery.answer(_["playcb_1"], show_alert=True)
-        except:
+        except Exception:
             return
     try:
         chat_id, channel = await get_channeplayCB(_, cplay, CallbackQuery)
-    except:
+    except Exception:
         return
     video = True if mode == "v" else None
     user_name = CallbackQuery.from_user.first_name
     await CallbackQuery.message.delete()
     try:
         await CallbackQuery.answer()
-    except:
+    except Exception:
         pass
     mystic = await CallbackQuery.message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
     try:
-        details, track_id = await YouTube.track(vidid, True)
+        details, track_id = await Platform.youtube.track(vidid, True)
     except Exception:
         return await mystic.edit_text(_["play_3"])
     ffplay = True if fplay == "f" else None
@@ -61,3 +61,4 @@ async def play_live_stream(client, CallbackQuery, _):
     else:
         return await mystic.edit_text("Not a live stream")
     await mystic.delete()
+            
